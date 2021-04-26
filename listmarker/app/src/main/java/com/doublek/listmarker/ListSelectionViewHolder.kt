acf08e46e4834d376186d65cafbbb313cf0ab4ac
tian.sun <suntian0@gmail.com>
2021-04-26 16:39:11 +0800

    add new app listsmark

---
+++ listmarker/app/src/main/java/com/doublek/listmarker/ListSelectionViewHolder.kt
@@ -0,0 +1,8 @@
+package com.doublek.listmarker
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ListSelectionViewHolder(itemView: View) :
+    RecyclerView.ViewHolder(itemView) {
+}
